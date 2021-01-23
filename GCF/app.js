418c3c3aeffba70eab4f01be0934dc7057ae333b
Gregory Oakley-Stevenson <gregory@okonetwork.org.uk>
2021-01-23 19:09:42 +0000

    Log permissions for use

--- GCF/app.js
+++ GCF/app.js
@@ -7,9 +7,33 @@ module.exports = (app) => {
   app.on("issues.opened", async (context) => {
     console.log("New issue opened");
     console.log(JSON.stringify(context.payload));
+
+    permissions = octokit.repos.getCollaboratorPermissionLevel({
+      owner: context.payload.repository.owner.login,
+      repository: context.payload.repository.full_name,
+      username: context.payload.issue.user.login
+    });
+    console.log(permissions);
     
-    return context.octokit.issues.createComment(
-      context.issue({ body: JSON.stringify(context.payload) })
-    );
+    releaseLabel = context.payload.issue.labels.filter(function(label) {
+      return label.name == "release";
+    })
+    if (releaseLabel) {
+      console.log("Issue is release issue");
+      return context.octokit.issues.createComment(
+        context.issue({ body: `
+          Hi @${context.payload.issue.user.login}
+          Latest android version: x.x.x
+          Latest iOS version: x.x.x
+          
+          To set the version for this release reply saying /setVersion n.n.n
+          where n.n.n is the version number for this release.
+          ${JSON.stringify(permissions)}
+        ` })
+      );
+    } else {
+      console.log("Issue is not release issue, ignoring")
+      return;
+    }
   });
 };

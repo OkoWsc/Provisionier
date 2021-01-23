d84664ddbea965cc31b62d8598d048b0304952da
Gregory Oakley-Stevenson <gregory@okonetwork.org.uk>
2021-01-23 18:44:25 +0000

    Stringify payload for logging

--- GCF/app.js
+++ GCF/app.js
@@ -5,10 +5,11 @@ module.exports = (app) => {
   app.log("Yay! The app was loaded!");
 
   app.on("issues.opened", async (context) => {
-    console.log(context);
+    console.log("New issue opened");
+    console.log(JSON.stringify(context.payload));
     
     return context.octokit.issues.createComment(
-      context.issue({ body: "Hello, World!" })
+      context.issue({ body: JSON.stringify(context.payload) })
     );
   });
 };

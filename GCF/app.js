64d64df3e1019c9d59608ac4ee7e304b43f8b021
Gregory Oakley-Stevenson <gregory@okonetwork.org.uk>
2021-01-23 21:02:59 +0000

    Set webhook path

--- GCF/app.js
+++ GCF/app.js
@@ -6,13 +6,8 @@ var admin = require('firebase-admin');
 admin.initializeApp();
 var db = admin.firestore();
 
-module.exports = (app, { getRouter }) => {
+module.exports = (app) => {
   app.log("Yay! The app was loaded!");
-
-  const router = getRouter("/app");
-  router.get("/ping", (req, res) => {
-    res.send("pong");
-  });
 
   app.on("issues.opened", async (context) => {
     console.log("New issue opened");
